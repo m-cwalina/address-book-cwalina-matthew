2b3da8a280780df949c3735a834180323673e6b9
Matthew Cwalina <matthewcwalina@gmail.com>
2022-12-20 16:14:25 +0100

    Setup routes to handle login, logout, and signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
   has_secure_password
-  attr_accessible :email, :password, :password_confirmation
   validates_uniqueness_of :email
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-# Routes for authentication
-get 'signup', to: 'users#new', as: 'signup'
-get 'login', to: 'sessions#new', as: 'login'
-get 'logout', to: 'sessions#destroy', as: 'logout'
+  # Routes for authentication
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
-resources :users
-resources :sessions
+  resources :users
+  resources :sessions
+  # Route to for home page
+  root to: 'pages#home', as: :home
 end

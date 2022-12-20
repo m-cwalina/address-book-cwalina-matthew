744e3dcfb1073ec60b108f5b1ee5cebfbb797e30
Matthew Cwalina <matthewcwalina@gmail.com>
2022-12-20 16:22:45 +0100

    Setup models and db schema with relations and fields

---
+++ db/migrate/20221220152128_create_phone_numbers.rb
@@ -0,0 +1,11 @@
+class CreatePhoneNumbers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :phone_numbers do |t|
+      t.string :phone_number
+      t.text :comment
+      t.references :person, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,52 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_20_145641) do
+ActiveRecord::Schema.define(version: 2022_12_20_152128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "street"
+    t.string "town"
+    t.string "zip_code"
+    t.string "state"
+    t.string "country"
+    t.bigint "person_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["person_id"], name: "index_addresses_on_person_id"
+  end
+
+  create_table "emails", force: :cascade do |t|
+    t.string "email_address"
+    t.text "comment"
+    t.bigint "person_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["person_id"], name: "index_emails_on_person_id"
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.string "salutation"
+    t.string "first_name"
+    t.string "middle_name"
+    t.string "last_name"
+    t.string "SSN"
+    t.datetime "birth_date"
+    t.text "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "phone_numbers", force: :cascade do |t|
+    t.string "phone_number"
+    t.text "comment"
+    t.bigint "person_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["person_id"], name: "index_phone_numbers_on_person_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -22,4 +64,7 @@ ActiveRecord::Schema.define(version: 2022_12_20_145641) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "addresses", "people"
+  add_foreign_key "emails", "people"
+  add_foreign_key "phone_numbers", "people"
 end
